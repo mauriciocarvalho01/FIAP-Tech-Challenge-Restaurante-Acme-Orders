0f2da307e27debf3011089e366e1e201e35253a0
Arnoldo Silva <analisi@gmail.com>
2024-07-30 14:35:16 -0300

    chore: clean up docker-compose.yml and improve Pagarme gateway code

--- src/infra/gateways/pagarme.ts
+++ src/infra/gateways/pagarme.ts
@@ -1,23 +1,39 @@
 import { PaymentGateway } from "@/infra/gateways";
 
 export class Pagarme implements PaymentGateway {
-    async pixGenerate(order: PaymentGateway.Order): Promise<PaymentGateway.PixGenerateResponse> {
-        return await this.createOrderWithPix(order);
-    }
+  async pixGenerate(
+    order: PaymentGateway.Order
+  ): Promise<PaymentGateway.PixGenerateResponse> {
+    return await this.createOrderWithPix(order);
+  }
 
-    private createOrderWithPix(order: PaymentGateway.Order): Promise<PaymentGateway.PixGenerateResponse> {
-        return new Promise<PaymentGateway.PixGenerateResponse>((resolve) => {
-            // Aqui podemos implementat a lógica para gerar o PIX usando a API do Pagarme
-            // Por exemplo:
-            const pixUrl = "https://example.com/pix"; // URL gerada para o PIX
-            const pixCode = "1234567890"; // Código do PIX gerado
-            const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // Expira em 24 horas
-            resolve({
-                paymentMethod: 'Pix',
-                pixUrl,
-                pixCode,
-                expirationDate
-            })
-        })
-    }
+  private createOrderWithPix(
+    order: PaymentGateway.Order
+  ): Promise<PaymentGateway.PixGenerateResponse> {
+    return new Promise<PaymentGateway.PixGenerateResponse>((resolve) => {
+      // Aqui podemos implementat a lógica para gerar o PIX usando a API do Pagarme
+      // Por exemplo:
+      const pixUrl = "https://example.com/pix"; // URL gerada para o PIX
+      const pixCode = "1234567890"; // Código do PIX gerado
+      const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // Expira em 24 horas
+
+      fetch("https://wh449fc0adc0c917bfc8.free.beeceptor.com", {
+        method: "POST",
+        body: JSON.stringify({
+          ...order,
+          pixUrl,
+          pixCode,
+          expirationDate,
+        }),
+      });
+
+      resolve({
+        paymentMethod: "Pix",
+        pixUrl,
+        pixCode,
+        expirationDate,
+        ...order,
+      });
+    });
+  }
 }

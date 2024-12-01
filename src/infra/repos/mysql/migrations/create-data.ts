171d8b5cb2b4b37af2fcd34aff179a9d92d65866
Mauricio Carvalho <developer.mauricio1@gmail.com>
2024-12-01 19:30:18 -0300

    feat: add database and monitoring resources

--- src/infra/repos/mysql/migrations/create-data.ts
+++ src/infra/repos/mysql/migrations/create-data.ts
@@ -4,6 +4,17 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateData1683634567892 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const tokenHandler = new TokenHandler()
+
+     // Criação do banco de dados caso não exista
+     await queryRunner.query(`
+      CREATE DATABASE IF NOT EXISTS restaurante_acme_pedidos;
+    `);
+
+    // Selecionar o banco de dados
+    await queryRunner.query(`
+      USE restaurante_acme_pedidos;
+    `);
+
     // Categorias
     await queryRunner.query(`
       INSERT INTO categorias (categoria_id, nome, data_cadastro, data_atualizacao)
